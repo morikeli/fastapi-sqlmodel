e1fb3e1a73ce7b5f55f9be2d12fe4f5b57e02b49
magnus.coder <kelimbth@gmail.com>
2025-04-25 17:00:55 +0300

    first commit: test data

---
+++ data/data.py
@@ -0,0 +1,48 @@
+books = [
+    {
+        "id": 1,
+        "title": "Book 1",
+        "author": "Author 1",
+        "publisher": "Publisher 1",
+        "published_date": "2023-01-01",
+        "language": "English",
+        "page_count": 500,
+    },
+    {
+        "id": 2,
+        "title": "Book 2",
+        "author": "Author 2",
+        "publisher": "Publisher 2",
+        "published_date": "2023-02-01",
+        "language": "English",
+        "page_count": 300,
+    },
+    {
+        "id": 3,
+        "title": "Book 3",
+        "author": "Author 3",
+        "publisher": "Publisher 3",
+        "published_date": "2023-03-01",
+        "language": "English",
+        "page_count": 400,
+    },
+    {
+        "id": 4,
+        "title": "Book 4",
+        "author": "Author 4",
+        "publisher": "Publisher 4",
+        "published_date": "2023-04-01",
+        "language": "English",
+        "page_count": 600,
+    },
+    {
+        "id": 5,
+        "title": "Book 5",
+        "author": "Author 5",
+        "publisher": "Publisher 5",
+        "published_date": "2023-05-01",
+        "language": "English",
+        "page_count": 700,
+    },
+    
+]
