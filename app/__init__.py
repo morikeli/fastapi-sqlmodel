e8db21040afc8489046839e828dc22e6a2e3488a
magnus.coder <kelimbth@gmail.com>
2025-04-26 13:09:42 +0300

    __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from app.db.database import init_db
+from app.routers.routes import router as book_router
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+
+
+version = 'v1'
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print('Server is starting ...')
+    await init_db()
+    yield 
+    print('Server has shutdown!')
+
+app = FastAPI(
+    title='Bookly',
+    description='A simple book management API',
+    version=version,
+    lifespan=lifespan,
+)
+
+# include router
+app.include_router(book_router, prefix=f'/api/{version}', tags=['books'])
