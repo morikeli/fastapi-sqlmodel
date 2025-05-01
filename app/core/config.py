fc0e7fe4a6f4cb5a467fff4734f472a670d07fff
magnus.coder <kelimbth@gmail.com>
2025-05-01 12:27:29 +0300

    feature: added new attributes in Settings class

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,15 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+from typing import Optional
 
 
 class Settings(BaseSettings):
     DATABASE_URL: str
+    JWT_SECRET: str
+    JWT_ALGORITHM: str
+    REDIS_HOST: str
+    REDIS_PORT: int
+    REDIS_PASSWORD: Optional[str] = None
+
 
     model_config = SettingsConfigDict(
         env_file='.env',
