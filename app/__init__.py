5974e9f91b34dc114b2e47bc16675928738a8df7
magnus.coder <kelimbth@gmail.com>
2025-05-01 12:32:07 +0300

    feature: registered new routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from app.db.database import init_db
 from app.routers.books import router as book_router
+from app.routers.auth import router as auth_router
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
@@ -17,8 +18,9 @@ app = FastAPI(
     title='Bookly',
     description='A simple book management API',
     version=version,
-    lifespan=lifespan,
+    # lifespan=lifespan,
 )
 
 # include router
-app.include_router(book_router, prefix=f'/api/{version}', tags=['books'])
+app.include_router(auth_router, prefix=f'/api/{version}/auth', tags=['Authentication'])
+app.include_router(book_router, prefix=f'/api/{version}', tags=['Books'])
