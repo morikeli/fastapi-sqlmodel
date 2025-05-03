6f8a733465d7e52efdc071d0d1fd3671e56bb529
magnus.coder <kelimbth@gmail.com>
2025-05-03 12:37:33 +0300

    fix: deleted class `IDMixin` 🚮

--- app/models/base.py
+++ app/models/base.py
@@ -7,21 +7,6 @@ import sqlalchemy as sa
 import uuid
 
 
-class IDMixin:
-    """
-    Mixin to add an ID primary key to a model.
-    """
-    
-    id: str = Field(
-        default_factory=lambda: uuid.uuid4().hex,
-        sa_type=sa.String(50),
-        primary_key=True,
-        unique=True,
-        nullable=False,
-        
-    )
-
-
 class TimeStampMixin:
     """
     Mixin to add created_at and updated_at timestamps to a model.
