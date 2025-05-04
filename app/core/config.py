6bc5c3971b444748202efc55bbf6d28550eb126d
magnus.coder <kelimbth@gmail.com>
2025-05-04 11:28:29 +0300

    feat: added new env variables in Settings()

--- app/core/config.py
+++ app/core/config.py
@@ -6,10 +6,21 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     JWT_SECRET: str
     JWT_ALGORITHM: str
+    SECRET_KEY: str
+    DOMAIN: str
     REDIS_HOST: str
     REDIS_PORT: int
     REDIS_PASSWORD: Optional[str] = None
-
+    MAIL_USERNAME: str
+    MAIL_PASSWORD: str
+    MAIL_FROM: str
+    MAIL_PORT: int
+    MAIL_SERVER: str
+    MAIL_FROM_NAME: str
+    MAIL_STARTTLS: bool
+    MAIL_SSL_TLS: bool
+    USE_CREDENTIALS: bool
+    VALIDATE_CERTS: bool
 
     model_config = SettingsConfigDict(
         env_file='.env',
